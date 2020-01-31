3c199c6490754b962c305a1386ae73b903434d04
Tony Day <tonyday567@gmail.com>
2020-01-31 14:56:19 +1000

    hello there

---
+++ app/rando.hs
@@ -0,0 +1,88 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE NamedFieldPuns #-}
+
+import Protolude hiding (race, race_, runConcurrently, putStrLn)
+import Box
+import Game.Chess.UCI
+import qualified Streaming.Prelude as S
+import Control.Concurrent.Classy.Async
+import System.IO
+import System.Process
+import GHC.Base (String)
+
+startE :: FilePath -> [GHC.Base.String] -> IO (Handle, Handle, ProcessHandle)
+startE cmd args = do
+  (Just inH, Just outH, Nothing, procH) <- createProcess (proc cmd args) {
+      std_in = CreatePipe, std_out = CreatePipe
+    }
+  hSetBuffering inH LineBuffering
+  pure (inH, outH, procH)
+
+mkStockfishBox :: Engine -> Cont IO (Box STM Text Text)
+mkStockfishBox e =
+  Box <$>
+  (Box.putLine (inH e) & commitPlug) <*>
+  (Box.getLine (outH e) & emitPlug)
+
+-- > e <- start'' putStrLn "stockfish" []
+-- > let stockc = (Box.putLine (inH e) & commitPlug)
+-- > let stocke = (Box.getLine (outH e) & emitPlug)
+-- > with (emit . liftE <$> stocke) (print =<<)
+-- Just "Stockfish 10 64 BMI2 by T. Romstad, M. Costalba, J. Kiiski, G. Linscott"
+-- with (liftC <$> stockc) (\c -> void $ commit c "uci")
+
+
+main :: IO ()
+main = do
+  (i,o,p) <- startE "/Users/tonyday/haskell/dull/other/stockfish-10-bmi2" []
+  putStrLn ":engine started"
+  let echoq = Transducer $ \s -> s & S.takeWhile (/="q") --  & S.map ("echo: " <>)
+  let stockc = Box.putLine i & commitPlug
+  let wireIn = Box <$> stockc <*> eStdin'
+  let stocke = Box.getLine o & emitPlug
+  let wireOut = Box <$> cStdout' <*> stocke
+  let fileOut = commitLines "dull.log"
+  putStrLn ":adding wires"
+  Protolude.print =<< race
+    (etc () echoq (wireIn) >> putStrLn ":wireIn terminated")
+    (etc () echoq (wireOut <> (Box <$> fileOut <*> mempty)) >> putStrLn ":wireOut terminated")
+  putStrLn ":wires terminated"
+  terminateCarefully p
+
+-- https://stackoverflow.com/questions/8820903/haskell-how-to-timeout-a-function-that-runs-an-external-command
+-- https://stackoverflow.com/questions/29944344/timeout-for-parallel-running-command-calls-with-a-worker-pool-in-haskell
+
+-- terminate external process on exception, ignore if already dead.
+terminateCarefully :: ProcessHandle -> IO ()
+terminateCarefully pHandle = 
+    catch (terminateProcess pHandle) (\(_::IOException) -> return ())
+
+safeExec :: CreateProcess -> IO ExitCode
+safeExec cp =
+    bracketOnError 
+        (createProcess cp {std_out = CreatePipe})
+        (\(_,_,_,pHandle) -> terminateCarefully pHandle)  
+        (\(_,_,_,pHandle) -> do
+            -- Workaround for a Windows issue.
+            latch <- newEmptyMVar
+            res <- race
+               (do -- IO actions are uninterruptible on Windows :(
+                  takeMVar latch 
+                  waitForProcess pHandle
+                  )
+               -- Dummy interruptible action that   
+               -- receives asynchronous exceptions first
+               -- and helps to end the other action.
+               (onException 
+                   (do 
+                      putMVar latch () 
+                      -- runs forever unless interrupted
+                      runConcurrently empty)
+                   (terminateCarefully pHandle))
+            pure $ either identity (const ExitSuccess) res)
---
+++ readme.md
@@ -0,0 +1,37 @@
+dull
+===
+
+[![Build Status](https://travis-ci.org/tonyday567/dull.svg)](https://travis-ci.org/tonyday567/dull) [![Hackage](https://img.shields.io/hackage/v/dull.svg)](https://hackage.haskell.org/package/dull) [![lts](https://www.stackage.org/package/dull/badge/lts)](http://stackage.org/lts/package/dull) [![nightly](https://www.stackage.org/package/dull/badge/nightly)](http://stackage.org/nightly/package/dull) 
+
+```
+stack build --test --exec "$(stack path --local-install-root)/bin/dull" --file-watch
+```
+
+links
+===
+
+https://sourceforge.net/p/scid/wiki/TheBookWindow/ | Scid / Wiki / TheBookWindow
+https://www.stmintz.com/ccc/index.php?id=456790 | Computer Chess Club Archives
+
+polyglot
+---
+
+https://www.google.com/search?q=chess+creating+a+book+file&oq=chess+creating+a+book+file&aqs=chrome..69i57j33.4894j0j8&sourceid=chrome&ie=UTF-8 | chess creating a book file - Google Search
+http://www.talkchess.com/forum3/viewtopic.php?t=63635 | How to Make a .BIN opening book (for non programmers?) - TalkChess.com
+http://www.talkchess.com/forum3/viewtopic.php?t=69509&start=40 | Polyglot extension tool - Page 5 - TalkChess.com
+https://www.chessprogramming.org/PolyGlot | PolyGlot - Chessprogramming wiki
+https://github.com/ddugovic/polyglot | ddugovic/polyglot: ALPHA - Polyglot chess opening book program
+http://www.talkchess.com/forum3/viewtopic.php?t=69509&start=40 | Polyglot extension tool - Page 5 - TalkChess.com
+http://talkchess.com/forum3/viewtopic.php?t=71004 | Polyglot 64-bit - TalkChess.com
+http://talkchess.com/forum3/viewtopic.php?t=69509 | Polyglot extension tool - TalkChess.com
+
+chessIO
+---
+https://github.com/mlang/chessIO | mlang/chessIO: Fast haskell chess move generator library and console UCI frontend
+
+pgn-extract
+---
+file:///Users/tonyday/Downloads/pgn-extract/help.html#flag-summary | pgn-extract: a Portable Game Notation (PGN) manipulator
+
+pgn-extract -vgrob.vars -d dups.pgn -o t3.pgn t2.pgn
+
